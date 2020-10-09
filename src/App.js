c65be7414956f694430a0e7a9cbd2fa4a2b5ae15
amsohag007 <amsohag007@gmail.com>
2020-10-10 00:53:55 +0600

    debug & deploy firebasw

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
+    {/* ---------------navbar & banner---------- */}
       <Nav />
       <Banner />
-
+    {/* ---------------Row for netflix original special large design---------- */}
+    <div className="app__row">
       <Row
         title="Netfilx Originals"
         fetchUrl={requests.fetchNetflixOriginal}
         isLargeRow
       />
+      {/* ---------------Row---------- */}
       <Row title="Trending Now" fetchUrl={requests.fetchTranding} />
       <Row title="Top Rated" fetchUrl={requests.fethTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fectchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fectchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fectchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fectchRomanceMovies} />
-      <Row title="Documentary Movies" fetchUrl={requests.fectchDocumentaries} />
+      <Row title="Documentary Movies" fetchUrl={requests.fectchDocumentaries}/>
+      </div>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -9,35 +9,44 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl,setTrailerUrl]=useState("");
-
+  
+  ///fetch movie information from themoviedb api
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      //"https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"
+      //"https://api.themoviedb.org/3/{fetchUrl=>>>requests.fetchNetflixOriginal}"
       setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, [fetchUrl]); //id [empty] run once when row loads and dont run again
+  }, [fetchUrl]); //loads each time fetchUrl changes.if [empty] run once when row loads and dont run again
 
-  //console.log(movies);
+ // console.log(movies);
+
+ //fecth trailer from youtube using movie-trailer api
+  const handleClick=(movie)=>{
+  //  console.log(movie?.title + "clicked")
+  //  console.log(trailerUrl);
+
+    if(trailerUrl){
+      console.log(trailerUrl);
+      setTrailerUrl("");
+      console.log("after setTrailer set to null>>>" +trailerUrl);
+    }else{
+      movieTrailer(movie?.original_title || movie?.title || movie?.name)
+       .then(url =>{
+         const  urlParams= new URLSearchParams (new URL(url).search);
+        setTrailerUrl(urlParams.get('v'));
+       })
+       .catch((error) => console.log("trailer fetching error>>>" + error));
+    }
+  }
+  //pop out youtube player options
   const opts = {
     height:"390",
     width:"100%",
     playerVars:{
       autoplay:1,
-    }
-  }
-  const handleClick=(movie)=>{
-    if(trailerUrl){
-      setTrailerUrl('');
-    }else{
-      movieTrailer(movie?.name || "")
-       .then(url =>{
-         const  urlParams= new URLSearchParams (new URL(url).search);
-        setTrailerUrl(urlParams.get('v'));
-       })
-       .catch((error) => console.log(error));
     }
   }
 
@@ -53,11 +62,11 @@ function Row({ title, fetchUrl, isLargeRow }) {
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
-            alt={movie.name}
+            alt={movie.name || movie.title}
           />
         ))}
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
     </div>
   );
 }
