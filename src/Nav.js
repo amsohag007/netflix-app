c8bf8e4dd14bb8a9c47a2f37ec335d3ea9f872b5
amsohag007 <amsohag007@gmail.com>
2020-10-09 17:35:24 +0600

    1.adding navbar 2.adding animation using useEffect

---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import "./Nav.css"
+
+function Nav() {
+    const [show,handleShow] = useState(false);
+    useEffect(()=>{
+        window.addEventListener("scroll",() => {
+            if(window.scrollY>100){
+                handleShow(true);
+            }else handleShow(false)
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    },[])
+    return (
+        <div className={`nav ${show && "nav__black"}` }>
+         <img 
+         className="nav__logo"
+         src="https://www.freepnglogos.com/uploads/netflix-logo-history-32.png"
+         alt="NetFlix Logo"
+         />
+         <button className="nav__button">Sign Up</button>
+        </div>
+    )
+}
+
+export default Nav
